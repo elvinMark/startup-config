5e5b9df1ae0bfc70af4ab4e17b041f83bd74dcb6
elvinMark <elvin.mark.titech@gmail.com>
2021-10-29 01:43:27 +0900

    updating ..

--- install.sh
+++ install.sh
@@ -15,6 +15,11 @@ sudo apt install python3-pip
 pip3 install -r requirements.txt
 sudo apt install python3-opencv
 
+# NodeJS
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+nvm install --lts
+npm -g i yarn
+
 # Emacs
 sudo apt install emacs
 get clone https://github.com/elvinMark/emacs-config.git
@@ -22,3 +27,13 @@ cp ./emacs-config/.emacs ~/
 
 # Oh-my-ZSH Shell
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# Neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+mv nvim.appimage ~/.local/bin
+echo "alias nvim=~/.local/bin/nvim.appimage" | cat >> ~/.zshrc 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+mkdir ~/.config/nvim
+
