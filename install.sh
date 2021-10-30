dfe8feb0d1a0775250640e30a420e4bafff7d0ad
elvinMark <elvin.mark.titech@gmail.com>
2021-10-31 00:27:09 +0900

    updating requirementes:

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ npm -g i yarn
 
 # Emacs
 sudo apt install emacs
-get clone https://github.com/elvinMark/emacs-config.git
+git clone https://github.com/elvinMark/emacs-config.git
 cp ./emacs-config/.emacs ~/
 
 # Oh-my-ZSH Shell
@@ -36,4 +36,5 @@ echo "alias nvim=~/.local/bin/nvim.appimage" | cat >> ~/.zshrc
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir ~/.config/nvim
-
+git clone https://github.com/elvinMark/vim-config.git
+cp ./vim-config/init.vim ~/.config/nvim/
